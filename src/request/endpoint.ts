3de081e23d06a88ceefe3edae5a82691cf910a5c
오진수 <ohjinsu@codersproduct.com>
2022-07-30 23:13:13 +0900

    Make the path of endpoint nullable

--- src/request/endpoint.ts
+++ src/request/endpoint.ts
@@ -6,12 +6,12 @@ type Method = typeof METHOD_LIST[number];
 
 type EndpointArgs = {
   method: Method;
-  path: string;
+  path?: string;
 };
 
 export class EndpointMetadata extends ClassMetadata<EndpointArgs> {}
 
-export const Endpoint = (method: Method, path: string) => (target: any) =>
+export const Endpoint = (method: Method, path?: string) => (target: any) =>
   push(
     target.prototype,
     new EndpointMetadata({
@@ -20,12 +20,12 @@ export const Endpoint = (method: Method, path: string) => (target: any) =>
     }),
   );
 
-export const Get = (path: string) => Endpoint("GET", path);
+export const Get = (path?: string) => Endpoint("GET", path);
 
-export const Post = (path: string) => Endpoint("POST", path);
+export const Post = (path?: string) => Endpoint("POST", path);
 
-export const Put = (path: string) => Endpoint("PUT", path);
+export const Put = (path?: string) => Endpoint("PUT", path);
 
-export const Patch = (path: string) => Endpoint("PATCH", path);
+export const Patch = (path?: string) => Endpoint("PATCH", path);
 
-export const Delete = (path: string) => Endpoint("DELETE", path);
+export const Delete = (path?: string) => Endpoint("DELETE", path);
