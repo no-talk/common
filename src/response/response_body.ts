e5246ea5270cf86e721ef994ded38a10dac6bbc2
오진수 <ohjinsu@codersproduct.com>
2022-08-16 13:11:36 +0900

    Fix the target of the response-body metadata

--- src/response/response_body.ts
+++ src/response/response_body.ts
@@ -4,4 +4,4 @@ type ResponseBodyArgs = Record<string, never>;
 
 export class ResponseBodyMetadata extends PropertyMetadata<ResponseBodyArgs> {}
 
-export const ResponseBody = () => (target: any, name: string) => push(target.prototype, new PropertyMetadata({}, name));
+export const ResponseBody = () => (target: any, name: string) => push(target, new PropertyMetadata({}, name));
