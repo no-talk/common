3f11f3b964dc72a53864c9c9cc79ea92d91478fc
오진수 <ohjinsu@codersproduct.com>
2022-08-16 13:35:38 +0900

    Fix the response body args

--- src/response/response_body.ts
+++ src/response/response_body.ts
@@ -1,7 +1,18 @@
 import { PropertyMetadata, push } from "@notalk/core";
 
-type ResponseBodyArgs = Record<string, never>;
+type ResponseBodyArgs = {
+  readonly type: { new (): unknown };
+};
 
 export class ResponseBodyMetadata extends PropertyMetadata<ResponseBodyArgs> {}
 
-export const ResponseBody = () => (target: any, name: string) => push(target, new PropertyMetadata({}, name));
+export const ResponseBody = (type: { new (): unknown }) => (target: any, name: string) =>
+  push(
+    target,
+    new ResponseBodyMetadata(
+      {
+        type,
+      },
+      name,
+    ),
+  );
