079c97153e009ea95c251c2fe71b14131b744ae4
오진수 <ohjinsu@codersproduct.com>
2022-08-16 12:54:33 +0900

    Fix the snakecase metadata to be for class

--- src/response/snakecase.ts
+++ src/response/snakecase.ts
@@ -1,7 +1,7 @@
-import { PropertyMetadata, push } from "@notalk/core";
+import { ClassMetadata, push } from "@notalk/core";
 
 type SnakeCaseArgs = Record<string, never>;
 
-export class SnakeCaseMetadata extends PropertyMetadata<SnakeCaseArgs> {}
+export class SnakeCaseMetadata extends ClassMetadata<SnakeCaseArgs> {}
 
-export const SnakeCase = () => (target: any, name: string) => push(target, new SnakeCaseMetadata({}, name));
+export const SnakeCase = () => (target: any) => push(target.prototype, new SnakeCaseMetadata({}));
